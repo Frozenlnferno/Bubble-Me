8264033c71946a2bcaa8e9cbcea00518e30e0f6c
Justin Ouyang <jstnoy@gmail.com>
2025-02-08 18:24:37 -0600

    Separated CSS files into a folder

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages'
 import BubbleInfo from './pages/BubbleInfo'
-import './App.css'
+import './css/App.css'
 
 function App() {
   return (
--- src/pages/BubbleInfo.jsx
+++ src/pages/BubbleInfo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../css/bubbleinfo.css'
 
 const BubbleInfo = () => {
   return (
